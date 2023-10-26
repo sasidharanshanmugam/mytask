1b249600172dae873e9b46a75659924e707489f2
sasidharanshanmugam <104121907+sasidharanshanmugam@users.noreply.github.com>
2023-10-26 07:22:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+
+// Database configuration
+const dbConfig = {
+  host: 'your-database-host',
+  user: 'your-database-username',
+  password: 'your-database-password',
+  database: 'your-database-name',
+};
+
+// Create a connection to the database
+const connection = mysql.createConnection(dbConfig);
+
+// Attempt to connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+
+  console.log('Connected to the database');
+});
+
+// Close the database connection when your application is done
+connection.end();
+
+// Note: You should perform your database operations inside this callback
+
+// Handle any errors that occur during the connection
+connection.on('error', (err) => {
+  console.error('Database error:', err);
+  console.log('Database not connected');
+});
+
+
